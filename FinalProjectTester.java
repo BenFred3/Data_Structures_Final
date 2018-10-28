1657293940f2ddbe13bd9a23ff2f8ba576779cde
Benjamin Frederickson <44035456+BenFred3@users.noreply.github.com>
2018-10-27 19:43:17 -0500

    Second day of work
    Honestly I think this is almost a finished product. I could add some more features, but the ones I have are working properly and I cannot find anymore errors. I plan to go through it next week and double check my comments and make sure things I've added are correct. After that I would probably like to add something to do with files and improve upon how my code works (make it more efficient, cleaner, ect).

--- FinalProjectTester.java
+++ FinalProjectTester.java
@@ -3,9 +3,10 @@
  * This program is my final project for Data structures. It simulates a writing program with commands built in. It uses two data structures,
  * stack and linked list.
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-// CURRENT TIME: 4:01 (hours:minutes)
+// CURRENT TIME: 10:13 (hours:minutes)
 
 /*
 Sprint One- I would first re-create a stack and linked list implementation that uses strings. 
@@ -14,11 +15,11 @@ Along with writing the formalized proposal.
 
 Sprint Two- I would then create the basic word program that would display a welcome message then give a prompt to type. 
 As the user enters each word, the program would display above the current message type and accept any commands entered. 
-Along with starting to write the userâ€™s manual including screenshots.
+Along with starting to write the user’s manual including screenshots.
 
 Sprint Three- I would create my two methods for the word program called undo and redo. 
 I would add user input validation to ensure no errors would occur. 
-Along with finishing my formalized proposal and userâ€™s manual if not already done.
+Along with finishing my formalized proposal and user’s manual if not already done.
 
 Sprint Four - I would polish my program making sure I hadn't left out any features I had planned to and fix any errors that the program still had. 
 I would then write the lessons learned and conclusion/summary of my report.
@@ -28,108 +29,278 @@ public class FinalProjectTester
 {
 	public static void main(String[] args) throws stackEmptyException, linkedListFullException, linkedListEmptyException
 	{
-		int userInputIndex = 0, userDeleteIndex = 0;
-		String userInput = "", undoWord = "", redoWord = "", userInputInsert = "", deleteValue = "";
-		Boolean endProgram = false, triggered = false;
+		// Values used in main.
+		int userInputIndex = 0, userDeleteIndex = 0, undoWordIndex = 0, redoWordIndex = 0;
+		String userInput = "", undoWord = "", redoWord = "", userInputInsert = "", deleteValue = "", lastCommand = "", undoTwiceCheck = "";
+		Boolean endProgram = false, triggered = false, firstLoop = true;
 		Scanner userInputScan = new Scanner(System.in);
 		
+		// Data structures created.
 		LinkedList wordsTyped = new LinkedList();
 		Stack undo = new Stack();
 		Stack redo = new Stack();
 		
-		// Welcome the user to the program and prompt them to begin typing
+		// Welcome the user to the program and prompt them to begin typing.
 		System.out.println("Welcome to the writing program!\nKeywords: InsertWord, UndoWord, RedoWord, DeleteWord, EndProgram.");
 		System.out.println("Please begin typing at any time..");
-		System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
 		
-		do 
+		do // Do this.
 		{
-			// Reset the boolean that shows no commands have been entered.
-			triggered = false;
+			// Act like the loop is the first loop if the wordsType is zero.
+			if (wordsTyped.getSize() == 0)
+			{
+				firstLoop = true; // Set the first loop to true again.
+				System.out.println("- - - - - - - - - - - - - - - - - - - - - - -"); // Print this so output looks better.
+			}
+			// If it is the first loop then block commands and only accept words.
+			if (firstLoop == true)
+			{
+				firstLoop = false; // Set the firstLoop to false.
+				
+				userInput = userInputScan.nextLine(); // Get the user's next command or word typed.
+				
+				// If the users input equals a command then 
+				if (userInput.equals("InsertWord") || userInput.equals("UndoWord") || userInput.equals("RedoWord") || userInput.equals("DeleteWord"))
+				{
+					System.out.println("Please enter in a word before using a command.");
+				}
+				// Else if the user wants to end the program.
+				else if (userInput.equals("EndProgram"))
+				{
+					// Set both of the booleans to true.
+					triggered = true;
+					endProgram = true;
+				}
+				// Else insert the users word.
+				else 
+				{
+					// Insert the user's input and push the userInput on both stacks.
+					wordsTyped.insertion(userInput);
+					undo.push(userInput);
+					redo.push(userInput);
+				}
+			}
+			triggered = false; // Reset the boolean that shows no commands have been entered.
 			
-			// After the first word typed display the words typed.
+			// If the size of the linkedList is greater than one, then print out what the user has typed.
 			if (wordsTyped.getSize() > 0)
 			{
 				System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
 				System.out.println(wordsTyped.display());
 				System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
 			}
-			userInput = userInputScan.nextLine();
-
-			if (userInput.equals("InsertWord")) // TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			userInput = userInputScan.nextLine(); // Get the user's next command or word typed.
+			
+			// If the user types the command "InsertWord".
+			if (userInput.equals("InsertWord")) // Need to get UNDO and REDO working with this command, otherwise works as intended. 
 			{
+				// Set triggered to true since a command has occurred, set the undoTwiceCheck to InsertWord, and reset the userInput.
 				triggered = true;
+				undoTwiceCheck = "InsertWord";
+				userInputIndex = 0;
 				
-				System.out.print("Enter an index number: ");
-				userInputIndex = userInputScan.nextInt();
+				// Do this until the user's output is correct.
+				do
+				{
+					// Try this.
+					try 
+					{
+						// Ask the user for index number.
+						System.out.print("Enter an number: ");
+						userInputIndex = userInputScan.nextInt();
+					}
+					// If the user enters something other than a number catch the exception and ask them to enter a number.
+					catch (InputMismatchException ex)
+					{
+						System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+						System.out.println("Please enter a number..");
+						System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+					}
+					userInputScan.nextLine(); // Clear out the scanner.
+				} while(userInputIndex <= 0);
+				
+				// Ask the user for what word they want to put into the linkedList.
 				System.out.print("Type your string to insert into " + userInputIndex + ": ");
-				userInputScan.nextLine();
-				userInputInsert = userInputScan.nextLine();
+				userInputInsert = userInputScan.nextLine(); // Get the nextLine as the userInputInsert.
+				// Set the word and indexValue (minus one so it reflects an actual array) to the function insertionAtIndex.
 				wordsTyped.insertionAtIndex(userInputInsert, userInputIndex - 1);
 				
+				lastCommand = "InsertWord"; // Set the last command to InsertWord.
+				
+				// If undo and redo are bigger than zero then pop the top value for the new one.
+				if (undo.size() > 0)
+				{
+					undo.pop();
+				}
+				if (redo.size() > 0)
+				{
+					redo.pop();
+				}
+				
+				// Push the word onto the redo and undo stacks.
 				redo.push(userInputInsert);
 				undo.push(userInputInsert);
 			}
 			
-			if (userInput.equals("UndoWord")) // TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			if (userInput.equals("UndoWord"))
 			{
+				// Set triggered to true since a command has occurred.
 				triggered = true;
 				
-				undoWord = undo.pop();
-				
-				for (int i = 0; i < wordsTyped.getSize(); i++)
+				if (undoTwiceCheck.equals("UndoWord"))
 				{
-					if (undoWord.equals(wordsTyped.search(i)))
-					{
-						wordsTyped.delete(i);
-					}
+					System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+					System.out.println("You cannot Undo twice. Please enter a different command or a word..");
 				}
-				redo.push(undoWord);
+				// If the lastCommand was InsertWord, delete the inserted word.
+				else if (lastCommand.equals("InsertWord") || lastCommand.equals("RedoInsertWord"))
+				{
+					undoTwiceCheck = "UndoWord"; // Set the undoTwiceCheck to UndoWord.
+					
+					undoWord = undo.pop(); // Pop the top value off of undo.
+					undoWordIndex = userInputIndex - 1; // Set the undoWordIndex to the one previously used.
+					wordsTyped.delete(undoWordIndex);
+				}
+				// Else if the lastCommand was DeleteWord, insert the deleteWord back the same index.
+				else if (lastCommand.equals("DeleteWord"))
+				{
+					undoTwiceCheck = "UndoWord"; // Set the undoTwiceCheck to UndoWord.
+					
+					undoWord = undo.pop(); // Pop the top value off of undo.
+					undoWordIndex = userDeleteIndex - 1; // Set the undoWordIndex to the one previously used.
+					wordsTyped.insertionAtIndex(undoWord, undoWordIndex);
+				}
+				// Else delete the last typed word.
+				else
+				{
+					undoTwiceCheck = "UndoWord"; // Set the undoTwiceCheck to UndoWord.
+					
+					undoWord = undo.pop(); // Pop the top value off of undo.
+					// Get the size and delete the highest index value..
+					undoWordIndex = wordsTyped.getSize();
+					wordsTyped.delete(undoWordIndex);
+				}
+				redo.push(undoWord); // Push the undoWord onto the redo stack.
 			}
 			
-			if (userInput.equals("RedoWord")) // TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			if (userInput.equals("RedoWord"))
 			{
+				// Set triggered to true since a command has occurred and set the undoTwiceCheck to RedoWord.
 				triggered = true;
+				undoTwiceCheck = "RedoWord";
 				
-				redoWord = redo.pop();
-				wordsTyped.insertion(redoWord);
+				redoWord = redo.pop(); // Pop the top word of the redo stack.
 				
+				// If the lastCommand was InsertWord, insert another word at the same index.
+				if (lastCommand.equals("InsertWord"))
+				{
+					redoWordIndex = userInputIndex - 1;
+					wordsTyped.insertionAtIndex(redoWord, redoWordIndex);
+					
+					lastCommand = "RedoInsertWord"; // Set the last command to RedoInsertWord.
+				}
+				// If the lastCommand was UndoWord, insert another word at the same index.
+				else if (lastCommand.equals("UndoWord"))
+				{
+					redoWordIndex = undoWordIndex;
+					wordsTyped.insertionAtIndex(redoWord, redoWordIndex);
+				}
+				// Else insert the redoWord.
+				else
+				{
+					wordsTyped.insertion(redoWord);
+					lastCommand = "RedoWord"; // Set last command to RedoWord.
+				}
+				
+				// If undo and redo are bigger than zero then pop the top value for the new one.
+				if (undo.size() > 0)
+				{
+					undo.pop();
+				}
+				if (redo.size() > 0)
+				{
+					redo.pop();
+				}
+				
+				// Push the redoWord onto the redo and undo stacks.
 				redo.push(redoWord);
 				undo.push(redoWord);
 			}
 			
-			if (userInput.equals("DeleteWord")) // TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			if (userInput.equals("DeleteWord")) // Works correctly but need to fix UNDO.
 			{
+				// Set triggered to true since a command has occurred, set the undoTwiceCheck to DeleteWord, and reset the userInput.
 				triggered = true;
+				undoTwiceCheck = "DeleteWord";
+				userDeleteIndex = -1;
 				
-				System.out.print("Enter an index number: ");
-				userDeleteIndex = userInputScan.nextInt();
-
-				deleteValue = wordsTyped.search(userDeleteIndex);
+				// Do this until the user's output is correct.
+				do 
+				{
+					// Try this.
+					try
+					{
+						// Ask the user for an number to delete.
+						System.out.print("Enter a number: ");
+						userDeleteIndex = userInputScan.nextInt();
+					}	
+					// If the user enters something other than a number catch the exception and ask them to enter a number.
+					catch (InputMismatchException ex)
+					{
+						System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+						System.out.println("Please enter a number..");
+						System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+					}
+					userInputScan.nextLine(); // Clear the scanner.
+				} while(userDeleteIndex <= 0);
+				// If undo is bigger than zero then pop the top value for the new one.
+				if (undo.size() > 0)
+				{
+					undo.pop();
+				}
+				
+				// Get the value to delete for the undo function.
+				deleteValue = wordsTyped.search(userDeleteIndex - 1);
 				undo.push(deleteValue);
 				
-				wordsTyped.delete(userDeleteIndex);
+				wordsTyped.delete(userDeleteIndex - 1); // Go into the delete function.
+				
+				lastCommand = "DeleteWord"; // Set the last command to DeleteWord.
 			}
 			
 			// If the user's input is endProgram exit the do-while loop.
 			if (userInput.equals("EndProgram"))
 			{
+				// Set both of the booleans to true.
 				triggered = true;
-				
 				endProgram = true;
 			}
+			// Else if any other commands haven't been entered insert the userInput.
 			else if (triggered == false)
 			{
+				// If undo and redo are bigger than zero then pop the top value for the new one.
+				if (undo.size() > 0)
+				{
+					undo.pop();
+				}
+				if (redo.size() > 0)
+				{
+					redo.pop();
+				}
+				undoTwiceCheck = "Default"; // Reset the undoTwiceCheck.
+				lastCommand = "Default"; // Set the last command to default.
+				
+				// Insert the user's input and push the userInput on both stacks.
 				wordsTyped.insertion(userInput);
 				undo.push(userInput);
 				redo.push(userInput);
 			}
-		} while (endProgram == false);
+		} while (endProgram == false); // While endProgram is false.
 		
-		System.out.println(undo.printStack());
-		System.out.println("Stack Size: " + undo.size());
-		System.out.println("Returned: " + undo.pop());
+		// Tell the user thank you and that the program is ending. (MAYBE ADD SAVE TO FILE HERE?)
+		System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.println("Thanks for using the writing program. Please come again!");
 		
-		userInputScan.close();
+		userInputScan.close(); // Close the scanner.
 	}
 }
--- LinkedList.java
+++ LinkedList.java
@@ -3,14 +3,14 @@
  * This program creates array-based linked list that takes strings.
  */
 
-
+// Exception handling for when linkedList is empty or full.
 @SuppressWarnings("serial")
 class linkedListEmptyException extends Exception 
 {
 	@Override
 	public String getMessage()
 	{
-		return ("List is empty");
+		return ("The linked list is empty.");
 	}
 }
 
@@ -20,23 +20,25 @@ class linkedListFullException extends Exception
 	@Override
 	public String getMessage()
 	{
-		return ("List is full");
+		return ("The linked list is full.");
 	}
 }
 
 public class LinkedList
 {
 	// Initial values.
-	private static final int SIZE = 10;
+	private static final int SIZE = 10000;
 	private String linkedArray[];
 	private int size;
 	
-	public LinkedList() // Constructor to make the LinkedList.
+	// Constructor to make the LinkedList.
+	public LinkedList() 
 	{
 		linkedArray = (String[]) new String[SIZE];
 		size = 0;
 	}
 	
+	// Method to get the size of the linkedList.
 	public int getSize()
 	{
 		return size;
@@ -45,16 +47,18 @@ public class LinkedList
 	/**
 	 * This function takes a index value and prints it out.
 	 * @param index.
-	 * @return linkedArray founded with the search value.
+	 * @return linkedArray found with the search value.
 	 * @throws linkedListEmptyException.
 	 */
 	public String search (int index) throws linkedListEmptyException
 	{
-		if (getSize() == 0) // If the size is equal to zero return null.
+		 // If the size is equal to zero throw a exception.
+		if (getSize() == 0)
 		{
 			throw new linkedListEmptyException();
 		}
-		else // Returns the value.
+		// Else return the value.
+		else 
 		{
 			return linkedArray[index];
 		}
@@ -67,11 +71,13 @@ public class LinkedList
 	 */
 	public void insertion(String word) throws linkedListFullException
 	{
-		if (getSize() == SIZE) // If the array is full print this message.
+		// If the array is full print this message.
+		if (getSize() == SIZE) 
 		{
 			throw new linkedListFullException();
 		}
-		else // Else increase the size and set the value.
+		// Else increase the size and set the value.
+		else 
 		{
 			linkedArray[getSize()] = word;
 			size++;
@@ -85,26 +91,34 @@ public class LinkedList
 	 */
 	public void insertionAtIndex (String word, int indexValue)
 	{
-		// If the index value in the linked list is empty just insert the word into it.
-		if (linkedArray[indexValue].equals(null))
+		// If the linkedArray's indexvalue isn't null then do this.
+		if (linkedArray[indexValue] != null) 
 		{
-			linkedArray[indexValue] = word;
-			size++;
-		}
-		else 
-		{
-			// TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			// Set two values for the for loop. One of the initial word in the request indexValue and one for the word next to it.
 			String movingValue = linkedArray[indexValue];
 			String tempValue = linkedArray[indexValue + 1];
 			
 			// Set the indexValues neighbor to the next index value and continue doing so until the end of the linked list.
-			for (int i = indexValue; i < size; i++)
+			for (int i = 1; i < size + 1; i++)
 			{
-				linkedArray[indexValue + i] = movingValue;
-				movingValue = tempValue;
-				tempValue = linkedArray[indexValue + i];
+				int nextIndex = i + 1; // Get one more than the current i value each loop.
+				linkedArray[indexValue + i] = movingValue; // Set the moving value to the indexValue plus the current loop number.
+				
+				// If both of the values are null exit out of the for loop. There is nothing more to do.
+				if (movingValue == null && tempValue == null)
+				{
+					break; // Break out of the for loop.
+				}
+				movingValue = tempValue; // Set the movingValue to the current tempValue.
+				tempValue = linkedArray[indexValue + nextIndex]; // Get the next tempValue.
 			}
-			// Set the new value at the index value and increase the size.
+			// Set the word at the index value given and increase the size.
+			linkedArray[indexValue] = word;
+			size++;
+		}
+		// Else the index value in the linked list is empty, so just insert the word into it and increase the size.
+		else
+		{
 			linkedArray[indexValue] = word;
 			size++;
 		}
@@ -118,15 +132,18 @@ public class LinkedList
 	 */
 	public void delete(int index) throws linkedListEmptyException
 	{
-		if (getSize() == 0) // If the size is equal to zero print this message.
+		// If the size is equal to zero throw an exception.
+		if (getSize() == 0) 
 		{
 			throw new linkedListEmptyException();
 		}
-		else // Else delete the can from the index value.
+		// Else delete the word from the index value.
+		else 
 		{	
-			for (int i = index + 1; i < size - 1; i++) // Copies the numbers into the position.
+			// Copies the numbers into the position.
+			for (int i = index; i < size - 1; i++) 
 			{
-				// TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+				linkedArray[i] = linkedArray[i + 1];
 			}
 			// Set the last element to null and decrease the size.
 			linkedArray[getSize() - 1] = null;
@@ -134,20 +151,23 @@ public class LinkedList
 		}
 	}
 	
-	/**
+	/*
 	 * This function deletes the first value in the linked list.
 	 * @return linkedArray deleted element.
 	 * @throws linkedListEmptyException.
-	 */
+	 *
 	public String deletion() throws linkedListEmptyException
 	{
-		if (getSize() == 0) // If the array is empty print this message.
+		// If the array is empty throw an exception.
+		if (getSize() == 0) 
 		{
 			throw new linkedListEmptyException();
 		}
-		else // Else decrease the size and return the element deleted.
+		// Else decrease the size and return the element deleted.
+		else 
 		{
-			for (int i = 0; i < size - 1; i++) // Copies the numbers into the position.
+			// Copies the numbers into the position.
+			for (int i = 0; i < size - 1; i++)
 			{
 				linkedArray[i] = linkedArray[i + 1];
 			}
@@ -155,9 +175,10 @@ public class LinkedList
 			linkedArray[getSize() - 1] = null;
 			size--;
 			
+			// Return the linkedArray.
 			return linkedArray[size];
 		}
-	}
+	} */
 
 	/**
 	 * This function displays the entire array.
@@ -166,20 +187,26 @@ public class LinkedList
 	 */
 	public String display() throws linkedListEmptyException
 	{
+		// Strings for the display.
 		String arrayData = "";
 		String tempData = "";
-		if (getSize() == 0) // If the array is empty print this message.
+		
+		// If the array is empty print this message.
+		if (getSize() == 0) 
 		{ 
 			throw new linkedListEmptyException();
 		}
-		else // Else display the array.
+		// Else display the array.
+		else 
 		{
+			// For loop to get all the data from the linkedArray.
 			for (int i = 0; i < size; i++)
 			{
 				tempData = (linkedArray[i] + " ");
 				arrayData = arrayData + tempData;
 			}
 		}
+		// Return the data.
 		return arrayData;
 	}
 }
